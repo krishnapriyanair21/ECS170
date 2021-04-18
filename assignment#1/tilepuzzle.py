45fb5d949769679f7932d1dccf63d151db80e54c
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-17 23:32:07 -0700

    works for small puzzles

--- assignment#1/tilepuzzle.py
+++ assignment#1/tilepuzzle.py
@@ -1,22 +1,38 @@
-def tilePuzzle (start, goal):
-    print(start)
-    unList(start)
-    unList(goal)
-    createList(unList(start))
-    # print(slideRight([start], 0) + " swap right")
-    # print(slideLeft([start], 3) + " swap Left")
-    # return statesearch([start], goal, [])
+count = 0 # recursion for DFS
 
+def tilePuzzle (start, goal):
+    startList = unList(start)
+    goalList = unList(goal)
+    
+    #generateNewStates(startList)
+
+    # What the output should look like
+    result = reverse(statesearch([startList], goalList, []))
+    final = []
+    
+    for i in range(len(result)):
+        new = createList(result[i])
+        final.append(new)
+    print(final)
+
+#find space element
+def findZero(curr):
+    for i in range(len(curr)):
+        if curr[i] == '0':
+            return i
+    return -1
+
+#syntax for input
 def unList(start):
-    tempList = []
+    tempList = ""
     startLen = len(start)
     for i in range(startLen):
         if (start[i] >= '0' and start[i] < '9'):
-            tempList.extend(start[i])
-    # tempList.extend(start[0][1])
-    # tempList.extend(start[2])
+            tempList += start[i]
+            #tempList.extend(start[i])
     return tempList
 
+#syntax for output
 def createList(goal): # https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
     rows, cols = (3, 3)
     result = []
@@ -27,60 +43,85 @@ def createList(goal): # https://www.geeksforgeeks.org/python-using-2d-arrays-lis
             col.append(int(goal[ele]))
             ele += 1
         result.append(col)
-    print(result)
-            
+    return result
 
 def swapPositions(curr, pos1, pos2):
-    temp = curr[pos1]
-    curr[pos1] = curr[pos2]
-    curr[pos2] = temp
-    return curr
+    curr = list(curr)
+    # temp = curr[pos1]
+    # curr[pos1] = curr[pos2]
+    # curr[pos2] = temp
+    curr[pos1], curr[pos2] = curr[pos2], curr[pos1]
+    return ''.join(curr)
 
+# Slide functions with edge cases
 def slideRight(start, pos):
-    return swapPositions([start], pos, (pos + 1)) 
-def slideLeft(start, pos, switch):
-    return swapPositions([start], pos, (pos - 1)) 
-def slideUp(start, pos, switch):
-    return swapPositions([start], pos, (pos - 3)) 
-def slideDown(start, pos, switch):
-    return swapPositions([start], pos, (pos + 3)) 
+    if ((pos + 1) % 3 == 0):
+        return 
+    print(swapPositions(start, pos, (pos + 1)), " right" )
+    return swapPositions(start, pos, (pos + 1)) 
+def slideLeft(start, pos):
+    if (pos % 3 == 0):
+        return 
+    print(swapPositions(start, pos, (pos - 1)), " left" )
+    return swapPositions(start, pos, (pos - 1)) 
+def slideUp(start, pos):
+    if (pos < 3):
+        return 
+    print(swapPositions(start, pos, (pos - 3)), " up")
+    return swapPositions(start, pos, (pos - 3)) 
+def slideDown(start, pos):
+    if (pos > 5): 
+        return
+    print(swapPositions(start, pos, (pos + 3)), " down" )
+    return swapPositions(start, pos, (pos + 3)) 
 
 def statesearch(unexplored,goal,path):
+    global count
+    count += 1
+    if (count > 4):
+        count-= 1
+        print("too far")
+        return []
     if unexplored == []:
         return []
     elif goal == head(unexplored):
+        print (head(unexplored), " is goal and found")
         return cons(goal,path)
-    else:       
-        result = statesearch(generateNewStates(head(unexplored)),
-                             goal,
-                             cons(head(unexplored), path))
+    else:
+        newStates = generateNewStates(head(unexplored)) 
+        print(head(unexplored), " is current state")
+        print(path, " is path")
+        print(newStates, " are new states")
+        #check for repeating states
+        for i in newStates:
+            for j in path:
+                if i == j:
+                    print(i, " removed for ", j)
+                    print(path)
+                    newStates.remove(i)
+        result = statesearch(newStates, goal, cons(head(unexplored), path))
         if result != []:
+            count -= 1
             return result
         else:
             return statesearch(tail(unexplored),
                                goal,
                                path)
 
-
-# def generateNewRedJumps(currState):
-#     return generateNew(currState,0,"RB_","_BR")
-
-# def generateNewBlueSlides(currState):
-#     return reverseEach(generateNew(reverse(currState),0,"B_","_B"))
-
-# def generateNewBlueJumps(currState):
-#     return reverseEach(generateNew(reverse(currState),0,"BR_","_RB"))
-
 def reverseEach(listOfLists):
     result = []
     for st in listOfLists:
         result.append(reverse(st))
     return result
+
 def reverse(st):
     return st[::-1]
     
 def head(lst):
     return lst[0]
+
+def next(lst):
+    return lst[1]
 
 def tail(lst):
     return lst[1:]
@@ -93,9 +134,21 @@ def drop(n,lst):
 
 def cons(item,lst):
     return [item] + lst
-# def generateNewStates(currState):
-#     return (generateNewRedSlides(currState) + generateNewRedJumps(currState) +
-#             generateNewBlueSlides(currState) + generateNewBlueJumps(currState))
+
+def generateNewStates(currState):
+    posZero = findZero(currState)
+    temp = []
+    if (slideRight(currState, posZero) is not None):
+        temp.append(slideRight(currState, posZero))
+    if (slideLeft(currState, posZero) is not None):
+        temp.append(slideLeft(currState, posZero))
+    if (slideUp(currState, posZero) is not None):
+        temp.append(slideUp(currState, posZero))
+    if (slideDown(currState, posZero) is not None):
+        temp.append(slideDown(currState, posZero))
+    return temp
+
+
 
 if __name__ == '__main__':
     import sys
