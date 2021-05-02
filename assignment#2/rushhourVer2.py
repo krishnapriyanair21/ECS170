69169e2a25069b8828e5541b4586465e39751d76
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-02 13:04:24 -0700

    technically correct

--- assignment#2/rushhourVer2.py
+++ assignment#2/rushhourVer2.py
@@ -1,9 +1,10 @@
 class Node(object):
     #base class for all boards
-    def __init__(self, board, prev, f):
-        self.board = board
-        self.prev = prev
-        self.f = f
+    def __init__(self, board, prev, f ,g):
+        self.board = board #current game board
+        self.prev = prev #index of last element in the path
+        self.f = f # f value of current game board
+        self.g = g
 
     def goalStateFound(self):
         row3 = list(self.board[2])
@@ -11,47 +12,64 @@ class Node(object):
             if (row3[4] == 'X'):
                 return True
         else:
-            return false
+            return False
 
 def rushhour(heuristic, rawInput):
-    printBoard(rawInput)
-    generateNewStates(rawInput, heuristic,0)
-    # AStarSearch(heuristic, rawInput)
-    #print result here
+    path = AStarSearch(heuristic, rawInput)
+    if (path != []):
+        totalStatesExplored = path.pop()
+        path.reverse()
+        for i in range(len(path)):
+            printBoard(path[i])
+            print("\n")
+        print("Total Moves: ", len(path))
+        print("Total States Explored: ", totalStatesExplored )
+    else:
+        print("could not find solution")
 
+# AStarSearch Algorithm
 def AStarSearch(heuristic, board):
+    #first input setup
     stateExploredCounter = 0
     currLevel = 0
-    f = caculateFn(heuristic, board, currLevel)
-    currExploring = Node(board, None, f)
+    f = calculateFn(heuristic, board, currLevel)
+    currExploring = Node(board, None, f, currLevel)
     open = [currExploring]
     closed = []
-    while(open != []):
-        print(currLevel, "level counter")
-        currExploring = open.pop(0)
-        # put currExploring in closed
-        closed.append(currExploring)
-        if(currExploring.goalStateFound()):
-            print("GOOOOOOOOOOOOOOOOOOOOAAAAAAAAAAALLLLLLLLLLLLL")
-            #create new var path[]
-            # reverse from current to start via prev
-        else:
-            newStates = generateNewStates(currExploring,heurisitic,currLevel)
-        # loop next states
-            for i in range(len(newStates)):
-                if (newStates[i] in closed):
-                    continue
-                else:
 
-            # expand all possible states from currExploring (IN NEW VARS)
-            # if in closed continue
-            # ELSE
-                # generate heuristic
-                # place next states from currExploring in open
-            #increment stateExploredCounter
+    while(open != []):
+        # each new node set up         
+        currExploring = open.pop(0)
+        currLevel = currExploring.g + 1
+        stateExploredCounter += 1
+        closed.append(currExploring)
+        
+        # goal is found, traverse path via and append state explored count to return
+        if(currExploring.goalStateFound()):
+            path = []
+            path.append(currExploring.board)
+            while(currExploring.prev != None):
+                currExploring = currExploring.prev
+                path.append(currExploring.board) 
+            path.append(stateExploredCounter)
+            return path
+        #goal not found search through new possible states
+        else:
+            newStates = generateNewStates(currExploring, heuristic, currLevel)
+            for i in range(len(newStates)):
+                if (newStates[i] not in closed):
+                    if(newStates[i].board in sublist for sublist in open):
+                        continue
+                    else:
+                        open.append(newStates[i])
         #sort open by f(n)
-        currLevel += 1
-    return 
+        for i in range(len(open)):
+            open.sort(key = sendKeyToSort)
+    return [] 
+
+#sort with a key for open
+def sendKeyToSort(board):
+    return board.f
 
 #counts the number of different chars != X in row 3 starting from first X
 # heuristic == 0
@@ -73,17 +91,19 @@ def blockingHeuristic(board):
                     blocksCounter += 1
     if ('-' in blocksInRow):
         blocksCounter -= 1
-
     if (blocksCounter == 0): # check to reach goal
         return 0 ## h(n) = 0
     else:
         return (blocksCounter + 1) ## one plus the number of cars
 
-# counts the number of blocks in front of X and the number of steps X has to take to reach the end
-# if we've reached goal state h(n) = 0
-# This heuristic is similar to the blocking heuristic but also takes into account the number of final steps needed
-# Because these steps would have to be taken into account for the blocking heuristic and the blocking heuristic is admissable
-# this heuristic must be admissable as well
+# This heuristic counts the number of blocks in front of X and the number of steps X has to take to reach the end
+    # if we've reached goal state h(n) = 0
+    # else  h(n) = blockingObstacles + spacesUntilXReachesGoal + 1
+# This heuristic is similar to the blocking heuristic but also takes into account the number of final steps needed. 
+# This is improving the blocking heuristic because a state where X is closer to the goal is objectively better than 
+# a state farther from the goal. 
+# This heuristic is admissable because the same steps would have to be taken into account for the blocking heuristic 
+# and the blocking heuristic is admissable.
 # heuristic == 1
 def selfMadeHeuristic(board):
     traverseXRow = list(board[2])
@@ -95,7 +115,7 @@ def selfMadeHeuristic(board):
     #blocking heuristic
     for j in range(6):
         if (traverseXRow[j] == 'X'):
-            whereIsX = j
+            whereIsX = j + 1
             break
     for i in range(j , 6):
         if (traverseXRow[i] != 'X'):
@@ -115,7 +135,7 @@ def selfMadeHeuristic(board):
         return h
 
 # uses the heuristic and g to find what f(n) is 
-def caculateFn(heuristic, currBoard, g):
+def calculateFn(heuristic, currBoard, g):
     f = 0
     if (heuristic == 0):
         f = blockingHeuristic(currBoard) + g
@@ -140,12 +160,11 @@ def findAllCarsAndTrucks(board):
     visitedEle = []
     currEle = []
     obstaclesWithCoordinates = []
-    
-    # find all instances in board != 'X' or '-' and add to list with coordinate
+    # find all instances in board != '-' and add to list with coordinate
     for i in range(6):
         currRow = board[i]
         for j in range(6):
-            if (currRow[j] != 'X' and currRow[j] != '-'):
+            if (currRow[j] != '-'):
                 if (currRow[j] not in allObstacles):
                     allObstacles.append([currRow[j],(j,i)]) #add each instance w/ (x,y) coordinate
                     if(currRow[j] not in visitedEle):
@@ -160,24 +179,24 @@ def findAllCarsAndTrucks(board):
             if (visitedEle[i] == allObstacles[j][0]):
                 currEle.append(allObstacles[j][1])
         obstaclesWithCoordinates.append(currEle)
-    print(obstaclesWithCoordinates)
     return obstaclesWithCoordinates
     
 #default generate all states with game board (will use horizontal and vertical state generators)
-def generateNewStates(board,heurisitic,g):
+def generateNewStates(board,heurisitic, g):
     newStates = []
     verticalStates = []
     horizontalStates = []
-    obstaclesWithCoordinates = findAllCarsAndTrucks(board)
+    
+    obstaclesWithCoordinates = findAllCarsAndTrucks(board.board)
     for i in range(len(obstaclesWithCoordinates)):
         x1 = obstaclesWithCoordinates[i][1][0] # xcoordinate of first instance
         x2 = obstaclesWithCoordinates[i][2][0] # x coordinate of second instance
         if (x1 ==  x2): # vertical
-            verticalStatesTemp = generateVerticalStates(board, obstaclesWithCoordinates[i])
+            verticalStatesTemp = generateVerticalStates(board.board, obstaclesWithCoordinates[i])
             for j in range(len(verticalStatesTemp)):
                 verticalStates.append(verticalStatesTemp[j])
         else: #horizontal
-            horizontalStatesTemp = generateHorizontalStates(board,obstaclesWithCoordinates[i])
+            horizontalStatesTemp = generateHorizontalStates(board.board,obstaclesWithCoordinates[i])
             for j in range(len(horizontalStatesTemp)):
                 horizontalStates.append(horizontalStatesTemp[j])
     
@@ -189,8 +208,8 @@ def generateNewStates(board,heurisitic,g):
 
 #formating for open and closed in AStar
 def createNode(state, heuristic, g, prev):
-    f = caculateFn(heuristic, state, g)
-    valToReturn = Node(state,prev,f)
+    f = calculateFn(heuristic, state, g)
+    valToReturn = Node(state,prev,f,g)
     return valToReturn
 
 #generate new states for a specific horizontal car or truck; returns all possible states moving that obstacle
@@ -222,31 +241,33 @@ def generateVerticalStates(board, obstacle):
 # returns slide one step to the right with the X and Y position of rightmost element
 def slideRight(posX, posY, board):
     currBoard = board.copy()
-    if (posX == 0):
+    if (posX == 5):
         return None
     rowToChange = currBoard[posY] 
     charLabel = rowToChange[posX]
-    if(rowToChange[posX - 1] != charLabel):
+    if(rowToChange[posX + 1] == '-'):
+        for i in range(len(rowToChange) - 2, -1, -1): # decrementing loop
+            if (rowToChange[i] == charLabel):
+                rowToChange = swap(rowToChange, i, i + 1)
+        currBoard[posY] = rowToChange
+    else:
         return None
-    for i in range(len(rowToChange) - 2, -1, -1): # decrementing loop
-        if (rowToChange[i] == charLabel):
-            rowToChange = swap(rowToChange, i, i + 1)
-    currBoard[posY] = rowToChange
     return currBoard
 
 # returns slide one step to the left with the X and Y position of leftmost element
 def slideLeft(posX, posY, board):
     currBoard = board.copy()
-    if (posX == 5):
+    if (posX == 0):
         return None
     rowToChange = currBoard[posY] 
     charLabel = rowToChange[posX]
-    if(rowToChange[posX + 1] != charLabel):
+    if(rowToChange[posX - 1] == '-'):
+        for i in range(len(rowToChange)):
+            if (rowToChange[i] == charLabel):
+                rowToChange = swap(rowToChange, i - 1, i)
+        currBoard[posY] = rowToChange
+    else:
         return None
-    for i in range(len(rowToChange)):
-        if (rowToChange[i] == charLabel):
-            rowToChange = swap(rowToChange, i - 1, i)
-    currBoard[posY] = rowToChange
     return currBoard
 
 
@@ -258,19 +279,20 @@ def slideUp(posX, posY, board):
     rowFirstEle = currBoard[posY] 
     charLabel = rowFirstEle[posX]
     #error handling
-    if (currBoard[posY + 1][posX] != charLabel):
+    if (currBoard[posY - 1][posX] == '-'):
+        for i in range(6):
+            searchString = list(currBoard[i])
+            if (searchString[posX] == charLabel):
+                swapUp = list(currBoard[i - 1])
+                searchString[posX] = swapUp[posX]
+                swapUp[posX] = charLabel
+                #revert to string to place in currBoard
+                swapUpStr = ''.join(swapUp)
+                searchStringtoStr = ''.join(searchString)
+                currBoard[i - 1] = swapUpStr
+                currBoard[i] = searchStringtoStr
+    else:
         return None
-    for i in range(6):
-        searchString = list(currBoard[i])
-        if (searchString[posX] == charLabel):
-            swapUp = list(currBoard[i - 1])
-            searchString[posX] = swapUp[posX]
-            swapUp[posX] = charLabel
-            #revert to string to place in currBoard
-            swapUpStr = ''.join(swapUp)
-            searchStringtoStr = ''.join(searchString)
-            currBoard[i - 1] = swapUpStr
-            currBoard[i] = searchStringtoStr
     return currBoard
 
 # returns slide down one step with the X and Y position of lowermost element
@@ -281,17 +303,18 @@ def slideDown(posX, posY, board):
     rowFirstEle = currBoard[posY] 
     charLabel = rowFirstEle[posX]
     #error handling
-    if (currBoard[posY - 1][posX] != charLabel):
+    if (currBoard[posY + 1][posX] == '-'):
+        for i in range(5, -1, -1): # changed second value to -1
+            searchString = list(currBoard[i])
+            if (searchString[posX] == charLabel):
+                swapDown = list(currBoard[i + 1])
+                searchString[posX] = swapDown[posX]
+                swapDown[posX] = charLabel
+                #revert to strings to place in currBoard
+                swapDownStr = ''.join(swapDown)
+                searchStringtoStr = ''.join(searchString)
+                currBoard[i + 1] = swapDownStr
+                currBoard[i] = searchStringtoStr
+    else:
         return None
-    for i in range(5, 0, -1):
-        searchString = list(currBoard[i])
-        if (searchString[posX] == charLabel):
-            swapDown = list(currBoard[i + 1])
-            searchString[posX] = swapDown[posX]
-            swapDown[posX] = charLabel
-            #revert to strings to place in currBoard
-            swapDownStr = ''.join(swapDown)
-            searchStringtoStr = ''.join(searchString)
-            currBoard[i + 1] = swapDownStr
-            currBoard[i] = searchStringtoStr
     return currBoard 
