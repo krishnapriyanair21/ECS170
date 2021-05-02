7b1a303b50ca1c53d644ebfc4cc01b6839267fa6
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-01 21:48:26 -0700

    full reorder and comments

--- assignment#2/rushhourVer2.py
+++ assignment#2/rushhourVer2.py
@@ -13,175 +13,11 @@ class Node(object):
         else:
             return false
 
-
 def rushhour(heuristic, rawInput):
     printBoard(rawInput)
     generateNewStates(rawInput, heuristic,0)
     # AStarSearch(heuristic, rawInput)
     #print result here
-
-def caculateFn(heuristic, currBoard, g):
-    f = 0
-    if (heuristic == 0):
-        f = blockingHeuristic(currBoard) + g
-    if (heuristic == 1):
-        f = selfMadeHeuristic(currBoard) + g
-    return f
-
-def printBoard(board):
-        for i in board:
-            print(i)
-
-def swap(curr, pos1, pos2):
-    curr = list(curr)
-    curr[pos1], curr[pos2] = curr[pos2], curr[pos1]
-    return ''.join(curr)
-
-#finds all the cars and trucks on the board and returns a list of lists with the char and coordinates
-def findAllCarsAndTrucks(board):
-    allObstacles = []
-    visitedEle = []
-    currEle = []
-    obstaclesWithCoordinates = []
-    
-    # find all instances in board != 'X' or '-' and add to list with coordinate
-    for i in range(6):
-        currRow = board[i]
-        for j in range(6):
-            if (currRow[j] != 'X' and currRow[j] != '-'):
-                if (currRow[j] not in allObstacles):
-                    allObstacles.append([currRow[j],(j,i)]) #add each instance w/ (x,y) coordinate
-                    if(currRow[j] not in visitedEle):
-                        visitedEle.append(currRow[j])
-    allObstacles.sort()
-    visitedEle.sort()
-
-    print(visitedEle)
-    
-    # create new list with ('CHAR', (set of coordinates)) 
-    for i in range(len(visitedEle)):
-        currEle = []
-        currEle.append(visitedEle[i])
-        for j in range(len(allObstacles)):
-            if (visitedEle[i] == allObstacles[j][0]):
-                currEle.append(allObstacles[j][1])
-        obstaclesWithCoordinates.append(currEle)
-    print(obstaclesWithCoordinates)
-    return obstaclesWithCoordinates
-    
-#default generate all states with game board
-def generateNewStates(board,heurisitic,g):
-    newStates = []
-    verticalStates = []
-    horizontalStates = []
-    obstaclesWithCoordinates = findAllCarsAndTrucks(board)
-    for i in range(len(obstaclesWithCoordinates)):
-        x1 = obstaclesWithCoordinates[i][1][0] # xcoordinate of first instance
-        x2 = obstaclesWithCoordinates[i][2][0] # x coordinate of second instance
-        if (x1 ==  x2):
-            verticalStatesTemp = generateVerticalStates(board, obstaclesWithCoordinates[i])
-            for j in range(len(verticalStatesTemp)):
-                verticalStates.append(verticalStatesTemp[j])
-        else:
-            horizontalStatesTemp = generateHorizontalStates(board,obstaclesWithCoordinates[i])
-            for j in range(len(horizontalStatesTemp)):
-                horizontalStates.append(horizontalStatesTemp[j])
-    
-    # make each new state into correct format for A* search
-    allStates = horizontalStates + verticalStates
-    for i in range(len(allStates)):
-        newStates.append(createNode(allStates[i],heurisitic,g,board)) 
-    return newStates
-
-#formating for open and closed in AStar
-def createNode(state, heuristic, g, prev):
-    f = caculateFn(heuristic, state, g)
-    valToReturn = Node(state,prev,f)
-    return valToReturn
-
-#generate new states for any horizontal car or truck
-def generateHorizontalStates(board, obstacle):
-    possibleStates = []
-    rightX = obstacle[-1][0]
-    rightY = obstacle[-1][1] 
-    if(slideRight(rightX,rightY,board) != None):
-        possibleStates.append(slideRight(rightX,rightY,board))
-    leftX = obstacle[1][0]
-    leftY = obstacle[1][1]
-    if(slideLeft(leftX,leftY,board) != None):
-        possibleStates.append(slideLeft(leftX,leftY,board))
-    return possibleStates
-
-#generate new states for any vertical car or truck
-def generateVerticalStates(board, obstacle):
-    possibleStates = []
-    downX = obstacle[-1][0]
-    downY = obstacle[-1][1] 
-    if(slideDown(downX,downY,board) != None):
-        possibleStates.append(slideDown(downX,downY,board))
-    upX = obstacle[1][0]
-    upY = obstacle[1][1]
-    if(slideUp(upX,upY,board) != None):
-        possibleStates.append(slideUp(upX,upY,board))
-    return possibleStates
-
-#counts the number of different chars != X in row 3 starting from first X
-def blockingHeuristic(board):
-    traverseXRow = list(board[2])
-    blocksInRow = []
-    blocksCounter = 0
-    whereIsX = 0
-
-    for j in range(6):
-        if (traverseXRow[j] == 'X'):
-            whereIsX = j
-            break
-    for i in range(j , 6):
-        if (traverseXRow[i] != 'X'):
-            if (traverseXRow[i] != '-'):
-                if (traverseXRow[i] not in blocksInRow):
-                    blocksInRow.append(traverseXRow[i])
-                    blocksCounter += 1
-    if ('-' in blocksInRow):
-        blocksCounter -= 1
-
-    if (blocksCounter == 0): # check to reach goal
-        return 0 ## h(n) = 0
-    else:
-        return (blocksCounter + 1) ## one plus the number of cars
-
-# counts the number of blocks in front of X and the number of steps X has to take to reach the end
-# if we've reached goal state h(n) = 0
-# This heuristic is similar to the blocking heuristic but also takes into account the number of final steps needed
-# Because these steps would have to be taken into account for the blocking heuristic and the blocking heuristic is admissable
-# this heuristic must be admissable as well
-def selfMadeHeuristic(board):
-    traverseXRow = list(board[2])
-    blocksInRow = []
-    blocksCounter = 0
-    whereIsX = 0
-    XToGoal = 0
-
-    for j in range(6):
-        if (traverseXRow[j] == 'X'):
-            whereIsX = j
-            break
-    for i in range(j , 6):
-        if (traverseXRow[i] != 'X'):
-            if (traverseXRow[i] != '-'):
-                if (traverseXRow[i] not in blocksInRow):
-                    blocksInRow.append(traverseXRow[i])
-                    blocksCounter += 1
-    if ('-' in blocksInRow):
-        blocksCounter -= 1
-
-    #define return value
-    XToGoal = (6 - j - 2) # 6-j is first X and there are 2 extra spots
-    h = XToGoal + blocksCounter + 1 ## one plus the number of cars plus how many moves until exit
-    if (blocksCounter == 0):
-        return 0 ## h(n) = 0
-    else:
-        return h
 
 def AStarSearch(heuristic, board):
     stateExploredCounter = 0
@@ -217,7 +53,173 @@ def AStarSearch(heuristic, board):
         currLevel += 1
     return 
 
-#slide one step to the right with the X and Y position of rightmost element
+#counts the number of different chars != X in row 3 starting from first X
+# heuristic == 0
+def blockingHeuristic(board):
+    traverseXRow = list(board[2])
+    blocksInRow = []
+    blocksCounter = 0
+    whereIsX = 0
+
+    for j in range(6):
+        if (traverseXRow[j] == 'X'):
+            whereIsX = j
+            break
+    for i in range(j , 6):
+        if (traverseXRow[i] != 'X'):
+            if (traverseXRow[i] != '-'):
+                if (traverseXRow[i] not in blocksInRow):
+                    blocksInRow.append(traverseXRow[i])
+                    blocksCounter += 1
+    if ('-' in blocksInRow):
+        blocksCounter -= 1
+
+    if (blocksCounter == 0): # check to reach goal
+        return 0 ## h(n) = 0
+    else:
+        return (blocksCounter + 1) ## one plus the number of cars
+
+# counts the number of blocks in front of X and the number of steps X has to take to reach the end
+# if we've reached goal state h(n) = 0
+# This heuristic is similar to the blocking heuristic but also takes into account the number of final steps needed
+# Because these steps would have to be taken into account for the blocking heuristic and the blocking heuristic is admissable
+# this heuristic must be admissable as well
+# heuristic == 1
+def selfMadeHeuristic(board):
+    traverseXRow = list(board[2])
+    blocksInRow = []
+    blocksCounter = 0
+    whereIsX = 0
+    XToGoal = 0
+
+    #blocking heuristic
+    for j in range(6):
+        if (traverseXRow[j] == 'X'):
+            whereIsX = j
+            break
+    for i in range(j , 6):
+        if (traverseXRow[i] != 'X'):
+            if (traverseXRow[i] != '-'):
+                if (traverseXRow[i] not in blocksInRow):
+                    blocksInRow.append(traverseXRow[i])
+                    blocksCounter += 1
+    if ('-' in blocksInRow):
+        blocksCounter -= 1
+
+    #define return value with added X movements
+    XToGoal = (6 - j - 2) # 6-j is first X and there are 2 extra spots
+    h = XToGoal + blocksCounter + 1 ## one plus the number of cars plus how many moves until exit
+    if (blocksCounter == 0):
+        return 0 ## h(n) = 0
+    else:
+        return h
+
+# uses the heuristic and g to find what f(n) is 
+def caculateFn(heuristic, currBoard, g):
+    f = 0
+    if (heuristic == 0):
+        f = blockingHeuristic(currBoard) + g
+    if (heuristic == 1):
+        f = selfMadeHeuristic(currBoard) + g
+    return f
+
+# prints board in correct format
+def printBoard(board):
+        for i in board:
+            print(i)
+
+#swap two elements in a list
+def swap(curr, pos1, pos2):
+    curr = list(curr)
+    curr[pos1], curr[pos2] = curr[pos2], curr[pos1]
+    return ''.join(curr)
+
+#finds all the cars and trucks on the board and returns a list of lists with the char and coordinates
+def findAllCarsAndTrucks(board):
+    allObstacles = []
+    visitedEle = []
+    currEle = []
+    obstaclesWithCoordinates = []
+    
+    # find all instances in board != 'X' or '-' and add to list with coordinate
+    for i in range(6):
+        currRow = board[i]
+        for j in range(6):
+            if (currRow[j] != 'X' and currRow[j] != '-'):
+                if (currRow[j] not in allObstacles):
+                    allObstacles.append([currRow[j],(j,i)]) #add each instance w/ (x,y) coordinate
+                    if(currRow[j] not in visitedEle):
+                        visitedEle.append(currRow[j])
+    allObstacles.sort()
+    visitedEle.sort()    
+    # create new list with ('CHAR', (set of coordinates)) 
+    for i in range(len(visitedEle)):
+        currEle = []
+        currEle.append(visitedEle[i])
+        for j in range(len(allObstacles)):
+            if (visitedEle[i] == allObstacles[j][0]):
+                currEle.append(allObstacles[j][1])
+        obstaclesWithCoordinates.append(currEle)
+    print(obstaclesWithCoordinates)
+    return obstaclesWithCoordinates
+    
+#default generate all states with game board (will use horizontal and vertical state generators)
+def generateNewStates(board,heurisitic,g):
+    newStates = []
+    verticalStates = []
+    horizontalStates = []
+    obstaclesWithCoordinates = findAllCarsAndTrucks(board)
+    for i in range(len(obstaclesWithCoordinates)):
+        x1 = obstaclesWithCoordinates[i][1][0] # xcoordinate of first instance
+        x2 = obstaclesWithCoordinates[i][2][0] # x coordinate of second instance
+        if (x1 ==  x2): # vertical
+            verticalStatesTemp = generateVerticalStates(board, obstaclesWithCoordinates[i])
+            for j in range(len(verticalStatesTemp)):
+                verticalStates.append(verticalStatesTemp[j])
+        else: #horizontal
+            horizontalStatesTemp = generateHorizontalStates(board,obstaclesWithCoordinates[i])
+            for j in range(len(horizontalStatesTemp)):
+                horizontalStates.append(horizontalStatesTemp[j])
+    
+    # make each new state into correct format for A* search
+    allStates = horizontalStates + verticalStates
+    for i in range(len(allStates)):
+        newStates.append(createNode(allStates[i],heurisitic,g,board)) 
+    return newStates
+
+#formating for open and closed in AStar
+def createNode(state, heuristic, g, prev):
+    f = caculateFn(heuristic, state, g)
+    valToReturn = Node(state,prev,f)
+    return valToReturn
+
+#generate new states for a specific horizontal car or truck; returns all possible states moving that obstacle
+def generateHorizontalStates(board, obstacle):
+    possibleStates = []
+    rightX = obstacle[-1][0]
+    rightY = obstacle[-1][1] 
+    if(slideRight(rightX,rightY,board) != None):
+        possibleStates.append(slideRight(rightX,rightY,board))
+    leftX = obstacle[1][0]
+    leftY = obstacle[1][1]
+    if(slideLeft(leftX,leftY,board) != None):
+        possibleStates.append(slideLeft(leftX,leftY,board))
+    return possibleStates
+
+#generate new states for a specific vertical car or truck; returns all possible states moving that specific obstacle
+def generateVerticalStates(board, obstacle):
+    possibleStates = []
+    downX = obstacle[-1][0]
+    downY = obstacle[-1][1] 
+    if(slideDown(downX,downY,board) != None):
+        possibleStates.append(slideDown(downX,downY,board))
+    upX = obstacle[1][0]
+    upY = obstacle[1][1]
+    if(slideUp(upX,upY,board) != None):
+        possibleStates.append(slideUp(upX,upY,board))
+    return possibleStates
+
+# returns slide one step to the right with the X and Y position of rightmost element
 def slideRight(posX, posY, board):
     currBoard = board.copy()
     if (posX == 0):
@@ -232,7 +234,7 @@ def slideRight(posX, posY, board):
     currBoard[posY] = rowToChange
     return currBoard
 
-#slide one step to the left with the X and Y position of leftmost element
+# returns slide one step to the left with the X and Y position of leftmost element
 def slideLeft(posX, posY, board):
     currBoard = board.copy()
     if (posX == 5):
@@ -248,7 +250,7 @@ def slideLeft(posX, posY, board):
     return currBoard
 
 
-#slide up one step with the X and Y position of uppermost element
+# returns slide up one step with the X and Y position of uppermost element
 def slideUp(posX, posY, board):
     currBoard = board.copy()
     if (posY == 0):
@@ -271,7 +273,7 @@ def slideUp(posX, posY, board):
             currBoard[i] = searchStringtoStr
     return currBoard
 
-#slide down one step with the X and Y position of lowermost element
+# returns slide down one step with the X and Y position of lowermost element
 def slideDown(posX, posY, board):
     currBoard = board.copy()
     if (posY == 5):
