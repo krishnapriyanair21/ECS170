a5dd40a80fafc96f0c26c6043ac547c33105eb06
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-04 13:03:09 -0700

    project 3 start

---
+++ assignment#3/hexapawn.py
@@ -0,0 +1,15 @@
+def hexapawn(board, boardSize, player, searchAhead):
+    return
+
+class hexapawnGame(object):
+    def __init__(self, board, size, player, searchAhead):
+        self.board = board
+        self.size = size
+        self.player = player
+        self.searchAhead = searchAhead
+    #board evaluation in class 
+
+# move generator
+
+#minimax search
+
