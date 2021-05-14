772f13f1438d6e08f51caa1c8bc932f6f5ca482e
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-13 17:55:26 -0700

    static eval set up

--- assignment#3/hexapawn.py
+++ assignment#3/hexapawn.py
@@ -1,20 +1,21 @@
 def hexapawn(board, boardSize, player, searchAhead):
     init = hexapawnGame(board, boardSize, player, searchAhead)
+    check = hexapawnGame(board,boardSize,player,searchAhead)
     #check = whiteMoveDown(init, 0, 0)
     # check = blackMoveUp(init, 2, 2)
-    # check = blackDiagonalRight(init, 0, 0)
-    check = whiteDiagonalLeft(init, 0, 1)
-    # check = whiteDiagonalRight(init, 2, 1)
-    # check = blackDiagonalLeft(init, 0, 0)
+    # check.board = blackDiagonalRight(init, 1, 2)
+    # check.board = whiteDiagonalLeft(init, 2, 1)
+    # check.board = whiteDiagonalRight(init, 0, 1)
+    # check.board = blackDiagonalLeft(init, 1, 2)
     print("initial board: ")
     init.printBoard()
     init.staticEval()
-    # print("new board: ")
-    # if (check):
-    #     print(check)
-    # else:
-    #     print("HAHA NOPE")
-    # return
+    print("new board: ")
+    if (check.board):
+        check.printBoard()
+    else:
+        print("HAHA NOPE")
+    return
 
 class hexapawnGame(object):
     def __init__(self, board, size, player, searchAhead):
@@ -26,6 +27,8 @@ class hexapawnGame(object):
     #board evaluation in class 
     def staticEval(self):
         score = 0
+        blackMarkers = 0
+        whiteMarkers = 0
         rowOne = list(self.board[0])
         rowThree = list(self.board[2])
         for i in range(3):
@@ -38,7 +41,14 @@ class hexapawnGame(object):
                 return #WHITE WINS
         # loop through board
             # count black and white pieces
-        
+        for i in range(self.size):
+            for j in range(self.size):
+                if (self.board[i][j] == 'b'):
+                    blackMarkers += 1
+                if (self.board[i][j] == 'w'):
+                    whiteMarkers += 1
+        print("White count: ", whiteMarkers)
+        print("Black count: ", blackMarkers)
         # set self.score = score and return it 
         return score
     def printBoard(self):
@@ -101,40 +111,101 @@ def blackMoveUp(currGame, posX, posY):
 
 def whiteDiagonalRight(currGame, posX, posY):
     board = (currGame.board).copy()
-    print("size: ", currGame.size)
     # check if current piece is w 
     if (board[posY][posX] != 'w'):
+        print("white right diagonal curr pos is not w")
         return None
+    # check if right diagonal is in range
     if((posX + 1) >= currGame.size) or ((posY + 1) >= currGame.size):
         print("Out of range: White Diagonal Right")
         return None
-    # check if right diagonal is in range
     # check if right diagonal is b
-
-    return board
+    if(board[posY + 1][posX + 1] == 'b'):
+        changeToDash = list(board[posY])
+        changeToDash[posX] = '-'
+        changeToW = list(board[posY + 1])
+        changeToW[posX + 1] = 'w'
+        changeToDashStr = ''.join(changeToDash)
+        changeToWStr = ''.join(changeToW)
+        board[posY] = changeToDashStr
+        board[posY + 1] = changeToWStr
+        return board
+    else: 
+        print("WhiteRightDiagonal is not black")
+        return None
 
 def whiteDiagonalLeft(currGame, posX, posY):
     board = (currGame.board).copy()
     # check if curr piece is w
     if (board[posY][posX] != 'w'):
+        print("White Diagonal Left curr pos is not w")
         return None
     # check if left diagonal is in range
     if((posX - 1) < 0) or ((posY + 1) >= currGame.size):
         print("Out of range: White Diagonal Left")
         return None
     # check if left diagonal is b
-    return board
+    if(board[posY + 1][posX - 1] == 'b'):
+        changeToDash = list(board[posY])
+        changeToDash[posX] = '-'
+        changeToW = list(board[posY + 1])
+        changeToW[posX - 1] = 'w'
+        changeToDashStr = ''.join(changeToDash)
+        changeToWStr = ''.join(changeToW)
+        board[posY] = changeToDashStr
+        board[posY + 1] = changeToWStr
+        return board
+    else: 
+        print("WhiteLeftDiagonal is not black")
+        return None
 
 def blackDiagonalRight(currGame, posX, posY):
     board = (currGame.board).copy()
     # check if curr piece is b
+    if (board[posY][posX] != 'b'):
+        print("Black right diagonal curr pos is not b")
+        return None
     # check if right diagonal is in range
+    if ((posX + 1) >= currGame.size) or ((posY - 1) < 0):
+        print("Out of Range: Black Diagonal Right")
+        return None
     # check if right diagonal is w
-    return board
+    if (board[posY - 1][posX + 1] == 'w'):
+        changeToDash = list(board[posY])
+        changeToDash[posX] = '-'
+        changeToW = list(board[posY - 1])
+        changeToW[posX + 1] = 'b'
+        changeToDashStr = ''.join(changeToDash)
+        changeToWStr = ''.join(changeToW)
+        board[posY] = changeToDashStr
+        board[posY - 1] = changeToWStr
+        return board
+    else:
+        print("Black Right Diagonal is not white")
+        return None
 
 def blackDiagonalLeft(currGame, posX, posY):
     board = (currGame.board).copy()
     # check if curr piece is b
+    if (board[posY][posX] != 'b'):
+        print("Black Diagonal Left curr pos is not b")
+        return None
     # check if left diagonal is in range
+    if ((posX - 1) < 0) or ((posY + 1) < 0):
+        print("Out of range: Black Diagonal Left")
+        return None
     # check if left diagonal is w 
+    if(board[posY - 1][posX - 1] == 'w'):
+        changeToDash = list(board[posY])
+        changeToDash[posX] = '-'
+        changeToW = list(board[posY - 1])
+        changeToW[posX - 1] = 'b'
+        changeToDashStr = ''.join(changeToDash)
+        changeToWStr = ''.join(changeToW)
+        board[posY] = changeToDashStr
+        board[posY - 1] = changeToWStr
+        return board
+    else: 
+        print("Black Left Diagonal is not white")
+        return None
     return board
