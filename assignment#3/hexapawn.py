27d6f41ed5033e1e96fb0fba4a65d6d84585fa0f
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-14 09:30:59 -0700

    white new states generator

--- assignment#3/hexapawn.py
+++ assignment#3/hexapawn.py
@@ -9,8 +9,8 @@ def hexapawn(board, boardSize, player, searchAhead):
     # check.board = whiteDiagonalRight(init, 0, 1)
     # check.board = blackDiagonalLeft(init, 1, 2)
     # print("initial board: ")
-    # init.printBoard()
-    # print(init.staticEval())
+    # # init.printBoard()
+    # print(init.score)
     # if (init.score > 0):
     #     print("score greater than 0")
     # # print("new board: ")
@@ -40,8 +40,8 @@ class hexapawnGame(object):
         self.curr = player
 
     # static evaluation function which sets score for hexapawn game object
-    # +10 if current player wins
-    # -10 if opponent wins
+    # +(size * 5) if current player wins
+    # -(size * 5) if opponent wins
     # else score is current player markers - opponent player markers
     def staticEval(self):
         blackMarkers = 0
@@ -67,38 +67,85 @@ class hexapawnGame(object):
                     whiteMarkers += 1
         if (self.player == 'w'):
             if(whiteLose):
-                self.score = -10
+                self.score = int(self.size)*(-5)
             elif (blackLose):
-                self.score =  10
+                self.score =  int(self.size)*5
             else:
                 self.score = int(whiteMarkers - blackMarkers)
         else:
             if (blackLose):
-                self.score =  -10
+                self.score =  int(self.size)*(-5)
             elif (whiteLose):
-                self.score =  10
+                self.score =  int(self.size)*5
             else:
                 self.score = int(blackMarkers - whiteMarkers)
-        # set self.score = score and return it 
-        print(self.score , " is score in static eval")
         return self.score
+
+    # For debugging
     def printBoard(self):
-        # for visual/debugging purposes
-        print("current player: ", self.player)
-        size = self.size
-        for i in range(size):
+        print("next player: ", self.curr)
+        for i in range(self.size):
             print(self.board[i])
     
 # move generator
+# once all moves are found create new hexapawnGame objects and switch the curr player
 def moveGenerator(currGame):
     if (currGame.curr == 'w'):
-        
-        currGame.curr = 'b'
+        white = findWhiteAndBlack(currGame, 'white')
+        possibleStates = whiteStates(white,currGame)
+        ## currGame.curr = 'b' 
+        for i in range(len(possibleStates)):
+            print("board ", i, ": ")
+            possibleStates[i].printBoard()
+        return possibleStates ## FIX THIS
     else:
+        black = findWhiteAndBlack(currGame, 'black')
         currGame.curr = 'w'
-    return []
+        return black
 #minimax search
 
+# Finds all states for white player
+# creates hexapawn object with new board and sets current (next) player to b
+# returns list of possible states
+def whiteStates(white,currGame):
+    possibleStates = []
+    for i in range(len(white)):
+        down = whiteMoveDown(currGame, white[i][0], white[i][1])
+        right = whiteDiagonalRight(currGame, white[i][0], white[i][1])
+        left = whiteDiagonalLeft(currGame, white[i][0], white[i][1])
+        if(down):
+            downHex = hexapawnGame(down,currGame.size,currGame.player,currGame.searchAhead)
+            downHex.curr = 'b'
+            possibleStates.append(downHex)
+        if(right):
+            rightHex = hexapawnGame(right,currGame.size,currGame.player,currGame.searchAhead)
+            rightHex.curr = 'b'
+            possibleStates.append(rightHex)
+        if(left):
+            leftHex = hexapawnGame(left,currGame.size,currGame.player,currGame.searchAhead)
+            leftHex.curr = 'b'
+            possibleStates.append(leftHex)
+    return possibleStates
+
+## Finds all white/black markers on the board
+## Returns a list of tuples with coordinates of player we are lookingToFind
+def findWhiteAndBlack(currGame, lookingToFind):
+    size = currGame.size
+    board = currGame.board.copy()
+    white = []
+    black = []
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == 'w':
+                white.append((j,i)) ## Coordinates in (x,y) format
+            if board[i][j] == 'b':
+                black.append((j,i)) ## Coordinates in (x,y) format
+    if(lookingToFind == 'black'):
+        return black
+    elif (lookingToFind == 'white'):
+        return white
+    else:
+        return None
 
 ## MOVEMENT FUNCTIONS: WHITE UP/RIGHT DIAGONAL/LEFT DIAGONAL
 ## MOVEMENT FUNCTIONS: BLACK DOWN/RIGHT DIAGONAL/LEFT DIAGONAL
@@ -107,6 +154,10 @@ def whiteMoveDown(currGame, posX, posY):
     # check that position is w
     if(board[posY][posX] != 'w'):
         print("Bad coordinates")
+        return None
+    # check if out of range
+    if ((posY + 1) >= currGame.size):
+        print("out of range: White move up")
         return None
     # check if move possible
     if (board[posY + 1][posX] == '-'):
@@ -181,6 +232,10 @@ def blackMoveUp(currGame, posX, posY):
     # check that position is b
     if(board[posY][posX] != 'b'):
         print("Bad coordinates")
+        return None
+    # check if out of range
+    if ((posY - 1) < 0):
+        print("Out of Range: Black Move up")
         return None
     # check if move possible
     if (board[posY - 1][posX] == '-'):
