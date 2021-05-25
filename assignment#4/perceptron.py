5f14bac30009335b70a6cfdfd8499eea1ae2ded6
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-25 10:32:59 -0700

    assignment 4 start

---
+++ assignment#4/perceptron.py
@@ -0,0 +1,10 @@
+def perceptron(threshold, adjustment, weights, examples, passNum):
+    # threshold is an int
+    # adjustment is an int
+    # weights is a list of ints
+    # examples is a list of lists: [[ True?False , [0, 0, 0, 0, 0] ] , ...]
+    # passNum is int
+    return
+
+## function for weight adjustment
+## function for looping through examples and return true or false for each
